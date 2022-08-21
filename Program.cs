1c434c0666b09072528bca7572714f278db3b3dd
Daniel Diaz <111096195+DanielPBravo@users.noreply.github.com>
2022-08-21 08:28:12 -0500

    Update Program.cs
    Se elimina el código comentado en la parte inferior del Program.cs

--- Program.cs
+++ Program.cs
@@ -64,34 +64,3 @@ namespace Pruebas_Consola
         }
     }
 }
-//if (nota > 5 || nota < 0)
-//{
-//    Console.Write("No debe ser superior a 5 ni inferior a 0:V\n");
-//    nota = Convert.ToDecimal(Console.ReadLine());
-//    contador++;
-//}
-//else
-//{
-//    contador++;
-//}
-//if (porcentaje > 100 || acomuladorPorcentaje + porcentaje > 100)
-//{
-//    Console.Write($"El porcentaje de la nota ingresada es muy alto, no debe ser mayor a \n" + (100 - acomuladorPorcentaje));
-//    porcentaje = Convert.ToDecimal(Console.ReadLine());
-//    acomuladorPorcentaje += porcentaje;
-//    porcentaje = porcentaje / 100;
-//    nota = nota * porcentaje;
-//    acomuladorNota += nota;
-//    promedio = acomuladorNota;
-//    //promedioTotal += promedio;
-//}
-//else
-//{
-//    acomuladorPorcentaje += porcentaje;
-//    porcentaje = porcentaje / 100;
-//    nota = nota * porcentaje;
-//    acomuladorNota += nota;
-//    promedio = acomuladorNota;
-//    //promedioTotal += promedio;
-
-//}
